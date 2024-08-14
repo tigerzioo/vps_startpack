325e2727781dde25bf9d2e61ec5d33697b312e67
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-08-14 11:04:48 -0500

    Update vpsstartpack.sh

--- vpsstartpack.sh
+++ vpsstartpack.sh
@@ -8,11 +8,28 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 apt-get install docker-compose
 
+echo "********更改时区到美国中部时区......"
+timedatectl set-timezone America/Chicago
+
 current_hostname=$(hostname)
 echo -e "当前主机名: $current_hostname"
 echo "------------------------"
-read -p "请输入新的主机名（直接回车不变动）: " new_hostname
+read -p "请输入新的主机名（直接回车不更改）: " new_hostname
 
-hostnamectl set-hostname "$new_hostname"
-sed -i "s/$current_hostname/$new_hostname/g" /etc/hostname
-systemctl restart systemd-hostnamed
+if [ -n "$new_hostname" ] && [ "$new_hostname" != "0" ]; then
+  hostnamectl set-hostname "$new_hostname"
+  sed -i "s/$current_hostname/$new_hostname/g" /etc/hostname
+  sed -i "1 s/localhost/localhost $new_hostname/" /etc/hosts
+  systemctl restart systemd-hostnamed
+else
+  echo "未更改主机名。"
+fi
+
+echo "------------------------"
+read -p "创建新用户（直接回车不创建）: " new_user
+if [ -n "$new_user" ] && [ "$new_user" != "0" ]; then
+  adduser "$new_user"
+  usermod -aG sudo "$new_user"
+else
+  echo "未创建用户。"
+fi
