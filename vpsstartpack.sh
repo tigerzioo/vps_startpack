563d3761d308f5e3d9bd3d3d8b4df1126cac0f43
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-08-17 21:00:00 -0500

    Fixed: MariaDB installation

--- vpsstartpack.sh
+++ vpsstartpack.sh
@@ -253,13 +253,11 @@ aptmariadb() {
     do
       if [[ "$REPLY" == 1 ]]; then
         echo "++++++++++安装 Mariadb 10.5 ...................."
-        exit
         apt install mariadb-server -y
         mysql_secure_installation
         break
       elif [[ "$REPLY" == 2 ]]; then
         echo "++++++++++安装 Mariadb 11.4 ...................."
-        exit
         curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version=11.4.3
         apt update
         apt -y install mariadb-server mariadb-client
