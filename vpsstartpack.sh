932d14e671a651ae245328c8a0d159eadd3955f2
tigerzioo <148376507+tigerzioo@users.noreply.github.com>
2024-09-28 15:16:46 -0500

    Fixed: Install Caddy

--- vpsstartpack.sh
+++ vpsstartpack.sh
@@ -395,6 +395,7 @@ aptcaddy() {
           curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
           # 添加 Caddy repository to sources list
           curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+          apt update
           apt install caddy -y
           systemctl enable caddy
           systemctl restart caddy
